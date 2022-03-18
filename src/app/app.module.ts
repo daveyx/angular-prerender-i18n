cc9d253e2d134bb95d254d4c61829da7e381c957
David <d.rehle@gmail.com>
2022-03-19 00:37:25 +0100

    BaseHrefPathLocationStrategy solution

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
-import { NgModule } from '@angular/core';
+import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { Injectable, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
+// avoid trailing slash
+// https://github.com/angular/angular/issues/21003#issuecomment-504777969
+@Injectable()
+export class BaseHrefPathLocationStrategy extends PathLocationStrategy {
+  override prepareExternalUrl(url: string) {
+    const baseHref = this.getBaseHref();
+    const extUrl = super.prepareExternalUrl(url);
+    return baseHref === extUrl ? Location.stripTrailingSlash(extUrl) : extUrl;
+  }
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserModule.withServerTransition({appId: 'serverApp'}),
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LocationStrategy,
+      useClass: BaseHrefPathLocationStrategy
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
